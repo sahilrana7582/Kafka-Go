31bbbcca0ac841ef581c815557c97d4a07e0439e
Sahil Rana <sahilrana27582@gmail.com>
2025-06-26 17:40:32 +0530

    Kafka In Go | Broker | Create Topic, Partitions Files, Retry Done

--- cmd/broker/main.go
+++ cmd/broker/main.go
@@ -5,7 +5,12 @@ import "github.com/sahilrana7582/kafka-in-go/internal/broker"
 func main() {
 	b := broker.NewBroker()
 
-	err := b.CreateTopic("test-topic-1", 3)
+	err := b.CreateTopic("test-topic", 3)
+	if err != nil {
+		panic(err)
+	}
+	// Create another topic with a different number of partitions
+	err = b.CreateTopic("another-topic", 5)
 	if err != nil {
 		panic(err)
 	}
--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -1,10 +1,12 @@
 package broker
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"sync"
+	"time"
 )
 
 const (
@@ -59,13 +61,14 @@ func (b *Broker) CreateTopic(name string, partitions int) error {
 func createFilesForTopicAndPartitions(topicName string, partitions int) {
 
 	basePath := filepath.Join("kafka-data", topicName)
-	err := os.MkdirAll(basePath, 0755)
-	if err != nil {
-		fmt.Printf("failed to create topic directory: %v\n", err)
+	if err := os.MkdirAll(basePath, 0755); err != nil {
+		fmt.Printf("❌ Failed to create topic directory: %v\n", err)
 		return
 	}
 
 	wg := sync.WaitGroup{}
+	const retryAttempts = 3
+	const retryDelayTime = 1 * time.Second
 
 	for i := 0; i < partitions; i++ {
 		wg.Add(1)
@@ -73,19 +76,40 @@ func createFilesForTopicAndPartitions(topicName string, partitions int) {
 		go func(partition int, wg *sync.WaitGroup) {
 			defer wg.Done()
 
-			filePath := filepath.Join(basePath, fmt.Sprintf("partition-%d.log", partition))
+			partitionPath := filepath.Join(basePath, fmt.Sprintf("partition-%d", partition))
 
-			file, err := os.Create(filePath)
+			file, err := RetryableFileCreate(partitionPath, retryAttempts, retryDelayTime)
 			if err != nil {
-				fmt.Printf("failed to create log file for partition %d: %v\n", partition, err)
+				fmt.Printf("❌ Failed to create file %s: %v\n", partitionPath, err)
 				return
 			}
 			defer file.Close()
 
-			fmt.Printf("Created %s\n", filePath)
+			fmt.Printf("✅ Created partition log: %s\n", partitionPath)
 		}(i+1, &wg)
 	}
 
 	wg.Wait()
 
+}
+
+func RetryableFileCreate(filePath string, retries int, delay time.Duration) (*os.File, error) {
+	var err error
+	var file *os.File
+
+	for attempt := 1; attempt <= retries; attempt++ {
+		file, err = os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_EXCL, 0644)
+		if err == nil {
+			return file, nil
+		}
+
+		if errors.Is(err, os.ErrExist) {
+			return nil, fmt.Errorf("file %s already exists", filePath)
+		}
+
+		fmt.Printf("[retry %d/%d] error creating %s: %v\n", attempt, retries, filePath, err)
+		time.Sleep(delay * time.Duration(attempt))
+	}
+
+	return nil, fmt.Errorf("failed to create %s after %d attempts: %w", filePath, retries, err)
 }
